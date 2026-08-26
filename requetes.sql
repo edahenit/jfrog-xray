348c238950a79821443f318e1626684a2e6456b9
EDENE Moh <moh.edene@gmail.com>
2026-08-26 17:44:21 +0200

    Update requetes.sql

--- requetes.sql
+++ requetes.sql
@@ -5,3 +5,28 @@ SELECT
         / current_setting('max_connections')::int, 2) AS pct_used
 FROM pg_stat_activity
 WHERE backend_type = 'client backend';
+
+SELECT
+  current_setting('max_connections')::int AS max_connections,
+  count(*) AS current_connections,
+  round(count(*) * 100.0
+        / current_setting('max_connections')::int, 2) AS pct_used
+FROM pg_stat_activity
+WHERE backend_type = 'client backend';
+
+SELECT state, count(*) FROM pg_stat_activity GROUP BY state;
+
+
+
+SELECT
+  pid,
+  usename,
+  now() - query_start AS duration,
+  state,
+  wait_event_type,
+  wait_event,
+  query
+FROM pg_stat_activity
+WHERE state = 'active'
+ORDER BY duration DESC
+LIMIT 10;
